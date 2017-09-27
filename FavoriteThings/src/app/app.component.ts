116c72e99a3af09cc11da3abd9734ad67a1f3b61
Coolone36 <michaeljlaritz@gmail.com>
2017-09-27 02:36:29 -0400

    Getting rid of update button and function automatic update of color

--- FavoriteThings/src/app/app.component.ts
+++ FavoriteThings/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 import * as firebase from 'firebase/app';
@@ -8,7 +8,7 @@ import * as firebase from 'firebase/app';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Favorite Things';
   favoriteColor = 'pink';
   favoriteNumber = 0;
@@ -17,18 +17,29 @@ export class AppComponent {
 
   }
 
+  ngOnInit(): void {
+    firebase.database().ref().child('color').on('value', (snapshot: firebase.database.DataSnapshot) => { 
+      this.favoriteColor = snapshot.val();
+     });
+  }
+
+  ngOnDestroy(): void {
+    // Good for best practice
+    firebase.database().ref().child('color').off();
+  }
+
   setColor(selectedColor: string): void {
     // console.log('You selected the color ' + selectedColor);
     // this.favoriteColor = selectedColor;
     // Don't need to set local variable since it will be access via cloud.
     firebase.database().ref().child('color').set(selectedColor);
   }
-  updateColor(): void {
-    console.log('TODO: Update the color');
-    firebase.database().ref().child('color').once('value', (snapshot: firebase.database.DataSnapshot) => { 
-      this.favoriteColor = snapshot.val();
-     });
-  }
+  // updateColor(): void {
+  //   console.log('TODO: Update the color');
+  //   firebase.database().ref().child('color').once('value', (snapshot: firebase.database.DataSnapshot) => { 
+  //     this.favoriteColor = snapshot.val();
+  //    });
+  // }
   setNumber(newFavoriteNumber: number): void {
     this.favoriteNumber = newFavoriteNumber;
   }
